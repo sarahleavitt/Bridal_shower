34d950fa0422586dfc229be6e19d1f9bdb9bb9ee
Sarah V. Leavitt <sv1205@bu.edu>
2020-05-20 13:26:32 -0400

    Added cards and answers to app

--- Bridal_Results_App.R
+++ Bridal_Results_App.R
@@ -6,24 +6,30 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 
-setwd("~/Michelle's Shower")
+setwd("~/Michelle's Shower/Bridal_shower")
 
 source <- "1ti-CttrCvS_pslaOyO3fHaDduVxGTtzUDz5yE9E_lpg"
 
 clean <- clean_results(source)
 answers <- clean[[1]]
 scores <- clean[[2]]
-advice <- clean[[3]]
+key_print <- clean[[3]]
+advice <- clean[[4]]
 
-winnerID <- scores %>%
-  group_by(Game) %>%
-  slice(1:3) %>%
-  select(Game, Points) %>%
-  unique(.)
+answers$Question <- gsub("\n$", "", add_newlines(answers$Question, 30))
+answers$Answer <- gsub("\n$", "", add_newlines(answers$Answer, 20))
+answers$Guest <- gsub("\n$", "", add_newlines(answers$Guest, 30))
 
-winners <- inner_join(scores, winnerID, by = c("Game", "Points"))
+adviceTab <- advice %>%
+  filter(Advice_display == "Yes") %>%
+  select(-Advice_display)
+adviceTab$Advice <- gsub("\n$", "", add_newlines(adviceTab$Advice, 100))
+names(adviceTab) <- c("", "")
 
-ui <- dashboardPage(
+
+games <- unique(answers$Game)
+
+ui <- dashboardPage(skin = "yellow",
   dashboardHeader(title = "Michelle's Bridal Shower Games!",
                   titleWidth = 450),
   dashboardSidebar(disable = TRUE),
@@ -31,93 +37,159 @@ ui <- dashboardPage(
     
     tabsetPanel(
       
-      tabPanel("How Well Do You Know the Bride?",
-              fluidRow(
-                box(
-                  title = "Answers",
-                  plotOutput("plot1", height = 700),
-                  width = 10
-                )
-              ),
-              fluidRow(
-                box(
-                  title = "Winners",
-                  tableOutput("table1"),
-                  width = 5
-                )
-              )
-      ),
-      
-      tabPanel("Bride or Groom? Guess Who Said It",
+      tabPanel(games[1],
+               
+               br(),
                fluidRow(
+                 column(4,
+                   imageOutput("card1", height = 600)
+                 ),
                  box(
-                   title = "Answers",
-                   plotOutput("plot2", height = 700),
-                   width = 10
+                   title = "Correct Answers",
+                   tableOutput("key1"),
+                   width = 4
+                   )
+                 ),
+               br(),
+                 
+                 fluidRow(
+                   box(
+                     title = "Guest Answers",
+                     girafeOutput("plot1", height = 800),
+                     width = 9
+                   ),
+                   box(
+                     title = "Guest Scores",
+                     tableOutput("table1"),
+                     width = 3
+                   )
                  )
                ),
+      
+      tabPanel(games[2],
+               
+               br(),
+               fluidRow(
+                 column(4,
+                        imageOutput("card2", height = 600)
+                 ),
+                 box(
+                   title = "Correct Answers",
+                   tableOutput("key2"),
+                   width = 3
+                 )
+               ),
+               br(),
+               
                fluidRow(
                  box(
-                   title = "Winners",
+                   title = "Guest Answers",
+                   girafeOutput("plot2", height = 1000),
+                   width = 9
+                 ),
+                 box(
+                   title = "Guest Scores",
                    tableOutput("table2"),
-                   width = 6
+                   width = 3
                  )
                )
       ),
 
-      tabPanel("How Old was the Bride-to-be?",
+      tabPanel(games[3],
+               
+               br(),
                fluidRow(
+                 column(4,
+                        imageOutput("card3", height = 600)
+                 ),
                  box(
-                   title = "Answers",
-                   plotOutput("plot3", height = 700),
-                   width = 10
+                   title = "Correct Answers",
+                   tableOutput("key3"),
+                   width = 2
                  )
                ),
+               br(),
+               
                fluidRow(
                  box(
-                   title = "Winners",
+                   title = "Guest Answers",
+                   girafeOutput("plot3", height = 1000),
+                   width = 9
+                 ),
+                 box(
+                   title = "Guest Scores",
                    tableOutput("table3"),
-                   width = 6
+                   width = 3
                  )
                )
       ),
 
-      tabPanel("Would She Rather?",
+      tabPanel(games[4],
+               
+               br(),
                fluidRow(
+                 column(4,
+                        imageOutput("card4", height = 600)
+                 ),
                  box(
-                   title = "Answers",
-                   plotOutput("plot4", height = 700),
-                   width = 10
+                   title = "Correct Answers",
+                   tableOutput("key4"),
+                   width = 4
                  )
                ),
+               br(),
+               
                fluidRow(
                  box(
-                   title = "Winners",
+                   title = "Guest Answers",
+                   girafeOutput("plot4", height = 1500),
+                   width = 9
+                 ),
+                 box(
+                   title = "Guest Scores",
                    tableOutput("table4"),
-                   width = 6
+                   width = 3
                  )
                )
       ),
 
-      tabPanel("Emoji Pictionary",
+      tabPanel(games[5],
+               
+               br(),
                fluidRow(
+                 column(4,
+                        imageOutput("card5", height = 600)
+                 ),
                  box(
-                   title = "Answers",
-                   plotOutput("plot5", height = 700),
-                   width = 10
+                   title = "Correct Answers",
+                   tableOutput("key5"),
+                   width = 4
                  )
                ),
+               br(),
+               
                fluidRow(
                  box(
-                   title = "Winners",
+                   title = "Guest Answers",
+                   girafeOutput("plot5", height = 600),
+                   width = 8
+                 ),
+                 box(
+                   title = "Guest Scores",
                    tableOutput("table5"),
-                   width = 6
+                   width = 4
                  )
                )
       ),
       
-      tabPanel("Advice",
-              h2("Wishes and Advice")
+      tabPanel("Wishes and Advice",
+               fluidRow(
+                 box(
+                   title = "Wishes and Advice",
+                   tableOutput("advice"),
+                   width = 10
+                 )  
+               )
       )
     )
   )
@@ -126,34 +198,104 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   
-  createPlot <- function(game){
-    gg <- ggplot(data = answers %>% filter(Game == game),
-                 aes(x = Answer, fill = Correct)) +
-      geom_bar() +
-      facet_wrap(~Question, scales = "free_y") +
-      scale_y_continuous(breaks = pretty_breaks(), name = "") +
-      scale_x_discrete(name = "") +
-      coord_flip() +
-      theme_gray(base_size = 14) +
-      theme(legend.title = element_blank(),
-            legend.position = "bottom") 
-    return(gg)
-  }
+  games <- unique(answers$Game)
+  cards <- c("Bride_Groom.jpg", "Pictionary.jpg", "How_old.jpg",
+             "How_well.jpg", "Would_she_rather.jpg")
   
-  output$plot1 <- renderPlot(createPlot("How Well Do You Know the Bride?"))
-  output$table1 <- renderTable(as.list(winners %>% filter(Game == "How Well Do You Know the Bride?")))
+  output$advice <- renderTable(adviceTab)
   
-  output$plot2 <- renderPlot(createPlot("Bride or Groom? Guess Who Said It"))
-  output$table2 <- renderTable(winners %>% filter(Game == "Bride or Groom? Guess Who Said It"))
+  ## GAME 1  
+  output$plot1 <- renderGirafe({
+    gg <- createPlot(answers, games[1])
+    girafe(ggobj = gg, pointsize = 14, width_svg = 15, height_svg = 15)
+  })
+  output$table1 <- renderTable(scores[scores$Game == games[1],
+                                            c("Guest", "Points", "Winner")])
+  
+  output$card1 <- renderImage({
+    return(list(src = paste0("./Game_cards/", cards[1]),
+                alt = "Card picture",
+                height = 600))
+    
+  }, deleteFile = FALSE)
+  
+  output$key1 <- renderTable(key_print[key_print$Game == games[1],
+                                         c("Question", "Correct Answer")])
+  
+  ## GAME 2
+  output$plot2 <- renderGirafe({
+    gg <- createPlot(answers, games[2])
+    girafe(ggobj = gg, pointsize = 14, width_svg = 15, height_svg = 15)
+  })
+  output$table2 <- renderTable(scores[scores$Game == games[2],
+                                      c("Guest", "Points", "Winner")])
+  
+  output$card2 <- renderImage({
+    return(list(src = paste0("./Game_cards/", cards[2]),
+                alt = "Card picture",
+                height = 600))
+    
+  }, deleteFile = FALSE)
+  
+  output$key2 <- renderTable(key_print[key_print$Game == games[2],
+                                       c("Question", "Correct Answer")])
+ 
+  ## GAME 3
+  output$plot3 <- renderGirafe({
+    gg <- createPlot(answers, games[3])
+    girafe(ggobj = gg, pointsize = 14, width_svg = 15, height_svg = 15)
+  })
+  output$table3 <- renderTable(scores[scores$Game == games[3],
+                                      c("Guest", "Points", "Winner")])
+  
+  output$card3 <- renderImage({
+    return(list(src = paste0("./Game_cards/", cards[3]),
+                alt = "Card picture",
+                height = 600))
+    
+  }, deleteFile = FALSE)
+  
+  output$key3 <- renderTable(key_print[key_print$Game == games[3],
+                                       c("Question", "Correct Answer")])
+  
+  
+  ## GAME 4
+  output$plot4 <- renderGirafe({
+    gg <- createPlot(answers, games[4])
+    girafe(ggobj = gg, pointsize = 14, width_svg = 15, height_svg = 15)
+  })
+  output$table4 <- renderTable(scores[scores$Game == games[4],
+                                      c("Guest", "Points", "Winner")])
+  
+  output$card4 <- renderImage({
+    return(list(src = paste0("./Game_cards/", cards[4]),
+                alt = "Card picture",
+                height = 600))
+    
+  }, deleteFile = FALSE)
+  
+  output$key4 <- renderTable(key_print[key_print$Game == games[4],
+                                       c("Question", "Correct Answer")])
+  
+  ## GAME 5
+  output$plot5 <- renderGirafe({
+    gg <- createPlot(answers, games[5])
+    girafe(ggobj = gg, pointsize = 14, width_svg = 15, height_svg = 15)
+  })
+  output$table5 <- renderTable(scores[scores$Game == games[5],
+                                      c("Guest", "Points", "Winner")])
+  
+  output$card5 <- renderImage({
+    return(list(src = paste0("./Game_cards/", cards[5]),
+                alt = "Card picture",
+                height = 600))
+    
+  }, deleteFile = FALSE)
+  
+  output$key5 <- renderTable(key_print[key_print$Game == games[5],
+                                       c("Question", "Correct Answer")])
 
-  output$plot3 <- renderPlot(createPlot("How Old was the Bride-to-be?"))
-  output$table3 <- renderTable(winners %>% filter(Game == "How Old was the Bride-to-be?"))
-
-  output$plot4 <- renderPlot(createPlot("Would She Rather?"))
-  output$table4 <- renderTable(winners %>% filter(Game == "Would She Rather?"))
-
-  output$plot5 <- renderPlot(createPlot("Emoji Pictionary"))
-  output$table5 <- renderTable(winners %>% filter(Game == "Emoji Pictionary"))
+  
 }
 
 shinyApp(ui, server)
--- Clean_Results.R
+++ Clean_Results.R
@@ -30,12 +30,15 @@ clean_results <- function(source){
                                        ifelse(grepl("Would Michelle", Question), "Would She Rather?",
                                               "How Well Do You Know the Bride?")))),
            Answer = ifelse(Game %in% c("How Well Do You Know the Bride?",
-                                       "Emoji Pictionary"), gsub(",", "", tolower(Answer)), Answer))
+                                       "Emoji Pictionary"), gsub(",", "", tolower(Answer)), Answer),
+           Answer = ifelse(Answer == "bride-to-be", "bride to be", Answer),
+           Number = as.numeric(gsub("[^0-9]", "", Question)))
   
   how_well <- long %>%
     filter(Game == "How Well Do You Know the Bride?") %>%
-    arrange(desc(Answer_key)) %>%
-    select(Game, Question, Answer)
+    arrange(desc(Answer_key), Question) %>%
+    select(Game, Question, Answer) %>%
+    filter(!duplicated(Answer))
   
   write_sheet(how_well, source, sheet = "How_well")
   
@@ -49,13 +52,18 @@ clean_results <- function(source){
     mutate(Correct = Key) %>%
     bind_rows(key_complex)
   
+  key_print <- key %>%
+    filter(!duplicated(Question)) %>%
+    select(Game, Question, `Correct Answer` = Key)
+  
   answers <- long %>%
     filter(Answer_key == FALSE) %>%
     select(-Answer_key) %>%
     left_join(key, by = c("Game", "Question", "Answer" = "Correct")) %>%
     filter(!is.na(Answer)) %>%
     mutate(Correct = ifelse(!is.na(Key), "Correct!", "Incorrect")) %>%
-    select(-Key)
+    select(-Key) %>%
+    arrange(Game, Number)
   
   scores <- answers %>%
     filter(Correct == "Correct!") %>%
@@ -63,10 +71,78 @@ clean_results <- function(source){
     summarize(Points = n()) %>%
     arrange(Game, desc(Points))
   
-  return(list(answers, scores, advice))
+  # Finding winners
+  winnerID <- scores %>%
+    group_by(Game) %>%
+    slice(1:2) %>%
+    select(Game, Points) %>%
+    unique(.)
+  
+  winners <- scores %>%
+    inner_join(winnerID, by = c("Game", "Points")) %>%
+    mutate(Winner = "Winner!")
+  
+  scores2 <- scores %>%
+    full_join(winners, by = c("Game", "Guest", "Points")) %>%
+    replace_na(list(Winner = ""))
+  
+  return(list(answers, scores2, key_print, advice))
 }
 
 
+createPlot <- function(df, game){
+  
+  # Ordering questions
+  plotData <- df %>% filter(Game == game)
+  questions <- unique(plotData$Question)
+  
+  plotData <- plotData %>%
+    mutate(Questionf = factor(Question, levels = c(questions[1],questions[2],questions[3],
+                                                   questions[4],questions[5],questions[6],
+                                                   questions[7],questions[8],questions[9],
+                                                   questions[10],questions[11],questions[12],
+                                                   questions[13],questions[14],questions[15])))
+  gg <- ggplot(data = plotData,
+               aes(x = Answer, fill = Correct)) +
+    geom_bar() +
+    facet_wrap(~Questionf, scales = "free_y", ncol = 3) +
+    scale_y_continuous(breaks = pretty_breaks(), name = "") +
+    scale_x_discrete(name = "") +
+    scale_fill_manual(breaks = c("Correct!", "Incorrect"),
+                       values = c("#FF689F", "darkgrey")) +
+    coord_flip() +
+    theme_gray(base_size = 14) +
+    theme(legend.title = element_blank(),
+          legend.position = "bottom")
+  
+  return(gg)
+}
+
+
+new_lines_adder = function(test.string, interval) {
+  #split at spaces
+  string.split = strsplit(test.string," ")[[1]]
+  # get length of snippets, add one for space
+  lens <- nchar(string.split) + 1
+  # now the trick: split the text into lines with
+  # length of at most interval + 1 (including the spaces)
+  lines <- cumsum(lens) %/% (interval + 1)
+  # construct the lines
+  test.lines <- tapply(string.split,lines,function(line)
+    paste0(paste(line,collapse=" "),"\n"),simplify = TRUE)
+  # put everything into a single string
+  result <- paste(test.lines,collapse="")
+  return(result)
+}
+
+add_newlines = function(x, interval) {
+  
+  # make sure, x is a character array   
+  x = as.character(x)
+  # apply splitter to each
+  t = sapply(x, FUN = new_lines_adder, interval = interval,USE.NAMES=FALSE)
+  return(t)
+}
 
 
 
